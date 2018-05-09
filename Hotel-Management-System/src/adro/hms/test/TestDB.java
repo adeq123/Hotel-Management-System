930fba31e36d7358e718b1945150dc08349849bd
adeq123 <adrroguski@gmail.com>
2018-05-09 15:47:20 +0200

    Initial commit

---
+++ Hotel-Management-System/src/adro/hms/test/TestDB.java
@@ -0,0 +1,34 @@
+package adro.hms.test;
+
+
+
+import java.sql.*;
+
+
+
+public class TestDB {
+
+	public static void main(String[] args) {
+		
+		try {
+			String query = "INSERT INTO guest VALUES ('7','Arnold','Norek','male','Passport','RFTasdasd9','888999777','Ciodrive 50', 'Standard 101', '3','2018-01-01') ";
+			String driver = "com.mysql.jdbc.Driver";
+			Class.forName(driver);
+			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/hotel_management_system?useSSL=false","hmsUser", "hmsUser");
+			PreparedStatement pstmt = conn.prepareStatement(query);
+			pstmt.executeUpdate();
+			System.out.println("OK!"
+					+ "");
+			conn.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+
+	}
+
+}
